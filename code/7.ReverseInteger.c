250a4854a36190ef86696d80f1f5c70be7e6956b
xingbofeng <503908971@qq.com>
2017-06-30 23:41:15 +0800

    13和7

---
+++ code/7.ReverseInteger.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int reverse(int x) {
+  long long reverseNum = 0;
+    while(x != 0) {
+      long long y = x % 10;
+      x = x / 10;
+      reverseNum = 10 * reverseNum + y;
+    }
+    return (reverseNum < INT_MIN || reverseNum > INT_MAX) ? 0 : reverseNum;
+}
+
+int main()
+{
+  return 0;
+}
